734a620c1b201202cc733b564931488f0813101a
harry2048 <gengwei1024@163.com>
2020-11-06 09:28:22 +0800

    雪花算法  全局流水号id

---
+++ src/test/java/com/baidu/redis2/Demo.java
@@ -0,0 +1,26 @@
+package com.baidu.redis2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo {
+    public static void main1(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        String name = (String) map.get("name");
+        if (null == name || "".equals(name)) {
+            name = "小花";
+        }
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        String s3 = new String("abc");
+        String s4 = s3.intern();
+
+        System.out.println(s1 == s2);// t
+        System.out.println(s1 == s3);// f
+        System.out.println(s1 == s4);// t
+    }
+}
